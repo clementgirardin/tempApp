7e0c52d34111b88c1d6dbb29b81fc85b1079fde2
giracle <clement.girardin@divtec.ch>
2022-11-30 15:04:20 +0100

    ajout message-erreur.js

--- src/store/magasin.js
+++ src/store/magasin.js
@@ -1,8 +1,18 @@
 
+import { api } from 'boot/axios';
+import {afficherMessageErreur} from "src/fonctions/message-erreur";
 
 // State : données du magasin
 const state = {
-
+  user: [
+    {
+      nom: "Test",
+      prenom: "Test",
+      email: "test@tempapi.divtec.me",
+      password: "Admlocal1",
+      is_admin: "0"
+    }
+  ]
 }
 
 /*
@@ -17,7 +27,22 @@ Actions : méthodes du magasin qui font appel aux mutations
 Elles peuvent être asynchrones !
  */
 const actions = {
-
+  getCapteurApi ({ commit }) {
+    // Exécuter la requête GET https://randomuser.me/api/?results=100&nat=CH
+    api.get('https://tempapi.divtec.me/api/capteurs')
+      // En cas de succès : remplacer le tableau des clients par la réponse de l'API
+      .then(function (response) {
+        console.log(response)
+        commit('setClients', response.data.results)
+      })
+      // En cas d'échec : afficher un message dans la console
+      .catch(function (error) {
+        afficherMessageErreur(
+          'Erreur lors de la récupération des clients !'
+        )
+        throw error
+      })
+  }
 }
 
 /*
