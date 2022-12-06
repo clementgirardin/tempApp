ef5a935f2bb52d7ad3f634d7c1a77cc0b5969293
giracle <clement.girardin@divtec.ch>
2022-12-06 14:41:31 +0100

    Changement de route

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '', component: () => import('pages/PageConnexion.vue') },
       { path: 'favoris', component: () => import('pages/CapteursFavoris.vue') },
-      { path: 'connection', component: () => import('pages/PageConnexion.vue') }
+      { path: 'apercu', component: () => import('pages/IndexPage.vue') }
     ]
   },
 
